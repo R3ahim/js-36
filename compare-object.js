204f13c212046d33414c8bde7ab8bcab08839dbd
R3ahim <r9ahim@gmail.com>
2022-02-27 00:36:28 +0600

    compare-object-finished

--- compare-object.js
+++ compare-object.js
@@ -7,11 +7,29 @@ if(first == third){
 else{
     // console.log('objects are different');
 };
-const hell = JSON.stringify(first);
-const hel = JSON.stringify(second)
-if(JSON.stringify(first) === JSON.stringify(second)){
-    console.log('there are aqual');
+// const hell = JSON.stringify(first);
+// const hel = JSON.stringify(second)
+
+const  first2 = {a:1, b:2};
+const second2 =  {b:2,a:1 };
+
+if(JSON.stringify(first2) === JSON.stringify(second2)){
+    // console.log('there are aqual');
 }
 else{
-    console.log('there are not aqual');
+    // console.log('there are not aqual');
 }
+function compareObjects(obj1, obj2) {
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+        return false;
+    }
+    for (const prop in obj1) {
+        if (obj1[prop] !== obj2[prop]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const isEqual = compareObjects(first2,second2);
+console.log(isEqual);
