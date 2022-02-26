d1379debc6911e8c7324759433c530e3db695ef8
R3ahim <r9ahim@gmail.com>
2022-02-27 00:13:33 +0600

    next more add

--- compare-object.js
+++ compare-object.js
@@ -1,9 +1,17 @@
-const first = {a:1};
-const second = {a:1};
+const first = {a:1, b:2};
+const second = {a:1,b:2};
 const third = first;
 if(first == third){
-    console.log('objects are equal')
+    // console.log('objects are equal')
 }
 else{
-    console.log('objects are different');
+    // console.log('objects are different');
+};
+const hell = JSON.stringify(first);
+const hel = JSON.stringify(second)
+if(JSON.stringify(first) === JSON.stringify(second)){
+    console.log('there are aqual');
+}
+else{
+    console.log('there are not aqual');
 }
